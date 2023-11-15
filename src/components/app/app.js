4f80fd95e7e32751b1cbf8d138415e7dd6e529ca
x-maria-x <m_khvatova99@mail.ru>
2023-11-15 21:53:21 +0700

    fix timer error

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -10,48 +10,7 @@ export default class App extends Component {
   minId = 0
 
   state = {
-    todoData: [
-      {
-        label: '0',
-        completed: false,
-        editing: false,
-        checked: false,
-        id: 0,
-        createTime: new Date(),
-        time: 5,
-        isTimerOn: true,
-      },
-      {
-        label: '1',
-        completed: false,
-        editing: false,
-        checked: false,
-        id: 1,
-        createTime: new Date(),
-        time: 5,
-        isTimerOn: true,
-      },
-      {
-        label: '2',
-        completed: false,
-        editing: false,
-        checked: false,
-        id: 2,
-        createTime: new Date(),
-        time: 5,
-        isTimerOn: true,
-      },
-      {
-        label: '3',
-        completed: false,
-        editing: false,
-        checked: false,
-        id: 3,
-        createTime: new Date(),
-        time: 5,
-        isTimerOn: true,
-      },
-    ],
+    todoData: [],
     filter: 'all',
   }
 
@@ -98,6 +57,7 @@ export default class App extends Component {
   }
 
   onToggleCompleted = (id) => {
+    this.timerOff(id)
     this.setState(({ todoData }) => {
       const idx = todoData.findIndex((el) => el.id === id)
       const oldTask = todoData[idx]
@@ -114,6 +74,7 @@ export default class App extends Component {
   }
 
   onToggleEditing = (id) => {
+    this.timerOff(id)
     this.setState(({ todoData }) => {
       const idx = todoData.findIndex((el) => el.id === id)
       const oldTask = todoData[idx]
@@ -150,46 +111,32 @@ export default class App extends Component {
   timerOn = (id, e) => {
     if (e) e.stopPropagation()
     const idx = this.state.todoData.findIndex((el) => el.id === id)
-    // const idx = id
     if (idx === -1) return
     const task = this.state.todoData[idx]
-    // if (!task) return
-    // const isTimerOn = task ? task.isTimerOn : false
-    // if (isTimerOn) return
-    // if (Object.keys(this.timers).includes(idx))
-    // console.log(Object.keys(this.timers))
-    // console.log(idx.toString())
-    // console.log(Object.keys(this.timers).includes(idx.toString()))
+
     if (Object.keys(this.timers).includes(id.toString())) return
 
     this.timers[id] = setInterval(() => {
       if (!this.state.todoData[idx]) return
 
-      this.setState(
-        ({ todoData }) => {
-          const oldTask = todoData[idx]
-          oldTask.time--
-          oldTask.isTimerOn = true
-          const newTask = { ...oldTask }
-          const newArr = [...todoData.slice(0, idx), newTask, ...todoData.slice(idx + 1)]
-          return {
-            todoData: newArr,
-          }
-        },
-        () => {
-          if (this.state.todoData[idx].time === 0) {
-            clearInterval(this.timers[idx])
-            delete this.timers[idx]
-            task.isTimerOn = false
-          }
+      this.setState(({ todoData }) => {
+        task.time--
+        if (!task.time) {
+          clearInterval(this.timers[idx])
+          delete this.timers[idx]
+          task.isTimerOn = false
+        } else {
+          task.isTimerOn = true
         }
-      )
+        return {
+          todoData: todoData.map((item) => (item.id === id ? task : item)),
+        }
+      })
     }, 1000)
   }
 
   timerOff = (id, e) => {
     if (e) e.stopPropagation()
-    // const { timers } = this.state
 
     clearInterval(this.timers[id])
     delete this.timers[id]
@@ -204,27 +151,10 @@ export default class App extends Component {
         todoData: newArr,
       }
     })
-    console.log(this.timers)
   }
 
   updateTimers() {
     Object.values(this.timers).forEach((timer) => clearInterval(timer))
-    // Object.keys(this.timers).forEach((taskId) => {
-    //   this.timerOff(taskId)
-    // clearInterval(this.timers[taskId])
-    // this.setState(({ todoData }) => {
-    //   console.log(todoData)
-
-    //   const newTodoData = [...todoData]
-    //   console.log(newTodoData)
-    //   newTodoData.forEach((task) => {
-    //     if (task.id === taskId) task.isTimerOn = false
-    //   })
-    //   return {
-    //     todoData: newTodoData,
-    //   }
-    // })
-    // })
     this.timers = {}
 
     this.state.todoData.forEach((item) => {
